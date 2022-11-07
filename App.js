1e9408a6c1a7035a2a0c10ae00a2dc08374dc815
hamza naseer <hamzanaseer711@gmail.com>
2022-11-07 20:28:57 +0500

    will continue later

--- App.js
+++ App.js
@@ -2,11 +2,15 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
 
+//navigation imports
+import { NavigationContainer } from "@react-navigation/native";
 export default function App() {
   return (
     <>
       <StatusBar style="light" />
-      <CategoriesScreen />
+      <NavigationContainer>
+        <CategoriesScreen />
+      </NavigationContainer>
     </>
   );
 }
